ebcaa55b63874fb292514f0c464ff1bee73a98df
azile6570-droid <azile6570-droid@users.noreply.github.com>
2026-08-18 09:30:51 +0300

    Initial project upload

---
+++ example.py
@@ -0,0 +1,20 @@
+def check_weather():
+    weather = "sunny"
+    if weather == "sunny":
+        print("It's a sunny day! Enjoy!")
+    else:
+        print("It might rain. Take an umbrella!")
+
+
+check_weather()
+
+
+def check_weather():
+    weather = "raining"
+    if weather == "sunny":
+        print("It's a sunny day! Enjoy!")
+    else:
+        print("It might rain. Take an umbrella!")
+
+
+check_weather()
